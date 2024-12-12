9937672ab08ae09fa9892a9615d8dc45f352f668
scuartas <scuartas@fiu.edu>
2024-12-12 00:27:40 -0500

    Add New transforms to launch file

--- demo_cpp_ready/launch/demo_launch.py
+++ demo_cpp_ready/launch/demo_launch.py
@@ -3,6 +3,23 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        #Transforms
+        Node (
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='rsLidar_tf',
+            arguments=['0.042', '0.0', '0.036', '1.570', '3.19', '-0.02', 'camera_link', 'camera_link'],
+            output='screen'
+        ),
+        Node (
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='rpLidar_tf',
+            arguments=['0.13', '0', '0', '0', '1.57', '0', 'tool0', 'laser'],
+            output='screen'
+        ),
+        
+        #UI Interface
         Node(
             package='demo_cpp_ready',
             executable='main_menu',
@@ -13,17 +30,20 @@ def generate_launch_description():
             executable='repair_interface',
             output='screen'
         ),
-        # Node(
-        #     package='demo_cpp_ready',
-        #     executable='surface_detection',
-        #     output='screen'
-        # ),
+        
+        #Mapping and Detection
+        Node(
+            package='demo_cpp_ready',
+            executable='lidar_scan',
+            output='screen'
+        ),
         
         Node(
             package='demo_py_ready',
             executable='surface_detection',
             output='screen'
         ),
+        #repair operations
         Node(
             package='demo_py_ready',
             executable='repair_grind',
