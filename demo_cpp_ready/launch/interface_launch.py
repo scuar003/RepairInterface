00f5b921f9cd667491d6f3e0ef664c6f49b31e3f
scuartas <scuartas@fiu.edu>
2025-03-28 19:50:48 -0400

    Update Cmake bug

---
+++ demo_cpp_ready/launch/interface_launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_cpp_ready',
+            executable='main_menu',
+            output='screen'
+        ),
+        Node(
+            package='demo_cpp_ready',
+            executable='repair_interface',
+            output='screen'
+        ),
+        
+        Node(
+            package='demo_py_ready',
+            executable='surface_detection',
+            output='screen'
+        ),
+        Node(
+            package='demo_py_ready',
+            executable='repair_grind',
+            output='screen'
+        ),
+        Node(
+            package='demo_py_ready',
+            executable='repair_vacuum',
+            output='screen'
+        ),
+        Node(
+            package='demo_py_ready',
+            executable='repair_expo_marker',
+            output='screen'
+        ),
+
+        # #moves
+        Node(
+            package='move_cmd_py',
+            executable='MoveHome',
+            output='screen'
+        ),
+        Node(
+            package='move_cmd_cpp',
+            executable='move3Dmouse',
+            output='screen'
+        ),
+    ])
